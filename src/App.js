87e1fee97b68c8b2f0d91127f02b27cd80b8adef
MartinWakaba <martin.wakaba@student.moringaschool.com>
2023-11-19 23:29:46 +0300

    displaying data from db

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import logo from './logo.svg';
-import { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import BotCollection from "./components/BotCollection";
+import BotArmy from "./components/BotArmy";
+import "./App.css";
 
 function App() {
-  const [bots, setBots] = useState([])
+  const [bots, setBots] = useState([]);
+  const [enlistedBots, setEnlistedBots] = useState([]); 
+
+  useEffect(() => {
+    // Fetch data from the API
+    fetch('http://localhost:3000/bots')
+    .then((resp) => resp.json())
+    .then((data) => setBots(data))
+    .catch((error) => console.error('Error fetching data:', error));
+}, []);
+
+const releaseFromArmy = (updatedEnlistedBots) => {
+  // Implement the logic to release bots from the army
+  setEnlistedBots(updatedEnlistedBots);
+};
+
+function handleBotDischarge(bot) {
+  // Remove the bot from enlistedBots in the frontend
+  const updatedEnlistedBots = enlistedBots.filter((enlistedBot) => enlistedBot.id !== bot.id);
+  setEnlistedBots(updatedEnlistedBots);
+}
  
-    useEffect(() => {
-      fetch('http://localhost:3000/bots')
-      .then(res => res.json())
-      .then(bots =>{ setBots(bots)
-      })
-    }, [])
-
-    
-
-    
-
-    
-
-
-  
-
 
   return (
-   <>
-   
-    
-   
-   </>
+    <div className="App">
+        <BotArmy 
+        enlistedBots={enlistedBots} 
+        releaseFromYourBotArmy={releaseFromArmy}/>
+
+      <BotCollection
+       bots={bots} 
+       enlistedBots={enlistedBots} 
+       setEnlistedBots={setEnlistedBots}
+       handleBotDischarge={handleBotDischarge}
+      />
+      
+    </div>
   );
 }
-
-export default App;
+export default App
